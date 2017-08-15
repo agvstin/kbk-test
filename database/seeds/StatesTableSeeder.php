98b06fd9497585c39745438c430ade3b8c49e027
Agustín Gutiérrez <agu.gutierrez@gmail.com>
2017-08-16 00:34:05 +0100

    State model, migration and seeder.

---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\State;
+
+class StatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	State::unguard();
+
+    	$fd = fopen(__DIR__ .'/states.csv', 'r');
+    	// skip header
+    	fgets($fd);
+
+    	while ($row = fgetcsv($fd)) {
+    		State::create([
+    			'id' => $row[0],
+    			'code' => $row[1],
+    			'name' => $row[2],
+    			'created_at' => $row[3],
+    			'updated_at' => $row[4],
+    		]);
+    	}
+    }
+}
